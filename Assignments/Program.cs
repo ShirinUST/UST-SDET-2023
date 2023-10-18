2d41a583d18c4eac01200d08f14b135191b96128
ShirinUST <shirin.safwanashiyas@ust.com>
2023-10-18 18:06:21 +0530

    First Assignment Commited

--- Assignments/Program.cs
+++ Assignments/Program.cs
@@ -1,1 +1,18 @@
 ﻿//18-10-2023
+
+using Assignments;
+
+Console.WriteLine("Enter Name:");
+string? name=Console.ReadLine();
+Console.WriteLine("Enter 3 Marks:");
+int markOne=Convert.ToInt32(Console.ReadLine());
+int markTwo = Convert.ToInt32(Console.ReadLine());
+int markThree = Convert.ToInt32(Console.ReadLine());
+Student student = new(name,markOne,markTwo,markThree);
+Console.WriteLine("Name : "+name);
+Console.WriteLine("Total Marks = "+student.GetTotalMarks());
+Console.WriteLine("Average Score = "+student.CalculateAverage());
+Console.WriteLine("Grade = "+student.GetGrade());
+
+
+
--- Assignments/Student.cs
+++ Assignments/Student.cs
@@ -8,5 +8,45 @@ namespace Assignments
 {
     internal class Student
     {
+        string? fullName;
+        int markOne,markTwo,markThree;
+
+        public Student(string? fullName, int markOne, int markTwo, int markThree)
+        {
+            this.fullName = fullName;
+            this.markOne = markOne;
+            this.markTwo = markTwo;
+            this.markThree = markThree;
+        }
+
+        public double CalculateAverage()
+        {
+            double average;
+            int totalMark;
+            totalMark = markOne + markTwo + markThree;
+            average = totalMark / 3;
+            return average;
+
+        }
+        public string GetGrade()
+        {
+            string? grade;
+            double avg=CalculateAverage();
+            if (avg > 90)
+                grade = "A+";
+            else if (avg > 80)
+                grade = "A";
+            else if (avg > 65)
+                grade = "B";
+            else if (avg > 50)
+                grade = "C";
+            else
+                grade = "F";
+            return grade;
+        }
+        public int GetTotalMarks()
+        {
+            return markOne + markTwo + markThree;
+        }
     }
 }
