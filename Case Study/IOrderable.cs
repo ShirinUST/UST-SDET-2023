7f0e0041394bb2ef2406a481e70c19663b011a76
ShirinUST <shirin.safwanashiyas@ust.com>
2023-10-28 17:52:16 +0530

    updated 28-10-2023

---
+++ Case Study/IOrderable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Case_Study
+{
+    internal interface IOrderable
+    {
+        public void PlaceOrder();
+        public void ProcessPayment();
+        public void DeliverProduct();
+
+    }
+}
